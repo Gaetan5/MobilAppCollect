782662043b5dfefe04a16054234b643f78d7f54c
Gaetan X Ekoro <43318443+Gaetan5@users.noreply.github.com>
2025-01-08 01:37:40 +0000

    Toujour Mr X

--- INTERACT/interact-website/.next/static/chunks/webpack.js
+++ INTERACT/interact-website/.next/static/chunks/webpack.js
@@ -129,7 +129,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("edaa2010fbd17b7a")
+/******/ 		__webpack_require__.h = () => ("c7696c9590b621b5")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- INTERACT/interact-website/src/components/Header.tsx
+++ INTERACT/interact-website/src/components/Header.tsx
@@ -4,10 +4,19 @@ import Link from 'next/link';
 const Header: React.FC = () => {
   return (
     <header className="bg-blue-500 text-white">
+      <div className="bg-blue-700 text-center py-10">
+        <h2 className="text-4xl font-bold">Welcome to INTERACT</h2>
+        <p className="text-xl mt-2">All in One Experience</p>
+      </div>
       <div className="container mx-auto flex justify-between items-center p-4">
         <h1 className="text-3xl font-bold">INTERACT</h1>
         <nav>
           <ul className="flex space-x-4">
+            <li>
+              <Link href="/" legacyBehavior>
+                <a className="text-lg font-semibold">Home</a>
+              </Link>
+            </li>
             <li>
               <Link href="/services" legacyBehavior>
                 <a className="text-lg font-semibold">Services</a>
@@ -35,10 +44,6 @@ const Header: React.FC = () => {
             </li>
           </ul>
         </nav>
-      </div>
-      <div className="bg-blue-700 text-center py-10">
-        <h2 className="text-4xl font-bold">Welcome to INTERACT</h2>
-        <p className="text-xl mt-2">All in One Experience</p>
       </div>
     </header>
   );
